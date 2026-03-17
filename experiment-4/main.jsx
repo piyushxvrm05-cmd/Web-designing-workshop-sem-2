2207227ce82c8918213351c31e5800aac8858ee9
aditya verma <piyushxvrm05@gmail.com>
2026-03-17 09:25:53 +0530

    commit exp 5

--- experiment-4/main.jsx
+++ experiment-4/main.jsx
@@ -1,39 +1,43 @@
 function calculateresult(){
-    let n=document.getElementById("subjects");
+    let n=document.getElementById("subjects").value;
     let total=0;
-    for (let i=1; i<=n; i++){
-        let marks=parseFloat(prompt("enter marks for subject"+1));
+    let marks;
+    for(let i=1;i<=n;i++){
+        marks=parseFloat(prompt("enter the marks of subjects"+i));
         total=total+marks;
     }
-    let average=Number;
-     average=total/n
-
-     let grade;
-     let result;
-
-     if(average>=90){
-        grade="A+";
-     }
-     else if(average>=75){
-        grade="A";
-     }
-     else if(average>=60){
-        grade="B+";
-     }
-     else if(average>=50){
-        grade="C";
-     }
-     else if(average>=40){
-        grade="D";
-     }
-     else{
-        grade="F";
-     }
+    let average=total/n;
+    let grade;
+    let result;
+    if(average>=90&&average<=100){
+       grade="grade A";
+    }
+    else if(average>=80&&average<=90){
+        grade="grade B";
+    }
+    else if(average>=70&&average<80){
+        grade="grade c";
+    }
+    else if(average>=60&&average<70){
+       grade="grade d";
+    }
+    else if(average>=50&&average<60){
+        grade="grade e";
+    }
+    else{
+        grade="grade f";
+    }
     if(average>=40){
-        result="Pass";
-     }
-     else{
+        result="pass";
+    }
+    else{
         result="fail";
-     }
-     
-}
+    }
+    document.getElementById("result").innerHTML=
+    "total marks: " + total+ 
+    "<br>"+"average marks:" + average.toFixed(2)+
+    "<br>"+"grade:"+grade +
+    "<br>"+"result:" +result;
+    }
+    
+    
