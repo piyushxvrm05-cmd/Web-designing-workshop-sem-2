d9898408185bd654303c9e63cbf67783491467e3
aditya verma <piyushxvrm05@gmail.com>
2026-04-09 19:53:22 +0530

    practical assesment

---
+++ experiment-6/main.jsx
@@ -0,0 +1,6 @@
+let heading=document.getElementById("mainheading");
+let paragrah=document.getElementById("paragraph");
+let input=Document.getElementById("userInput");
+
+let fontSize=16;
+document.getElementById("changeTextBtn").add
---
+++ practical_1/main.jsx
@@ -0,0 +1,14 @@
+
+    function calculate() {
+        let n = document.getElementById("num").value;
+        let sum = 0;
+
+        for(let i = 1; i <= n; i++) {
+            if(i % 2 === 0) {
+                sum += i;
+            }
+        }
+
+        document.getElementById("result").innerHTML =
+            "Sum of even numbers = " + sum;
+    }
